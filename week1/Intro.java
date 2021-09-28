184faecea0edd45ceb50dde6f01a6ab5ceb2f2e4
seonhan427 <80662706+seonhan427@users.noreply.github.com>
2021-09-28 12:43:32 +0900

    Add files via upload

---
+++ week1/Intro.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Intro {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요?");
+		System.out.println("저는 자바를 처음 공부합니다.");
+		System.out.println("저의 나이는 24살입니다.");
+
+
+		System.out.println("안녕하세요?\n저는 자바를 처음 공부합니다.\n저의 나이는 20살입니다.");
+	}
+}
+//TEXT
+/*TEXT*/
+/**DOCUMENTS*/
